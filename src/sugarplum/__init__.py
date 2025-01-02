308156e7e0d3f747895775a02b12cce858a0e1d0
Stephen Lutes <6340474+stephenlutes@users.noreply.github.com>
2025-01-01 20:30:00 -0800

    Refactor library to better seperate test inputs and answers

--- src/sugarplum/__init__.py
+++ src/sugarplum/__init__.py
@@ -1,54 +1,40 @@
 import os
 from pathlib import Path
-from typing import Any
 
 
-def _file_exists(year: int, day: int, part: int, sub_part: str = "") -> bool:
-    file_path: Path = (
-        Path(os.environ["SUGARPLUM_BASE_PATH"])
-        / f"{year}/{day:02}/test-{part}{sub_part}.in"
-    )
-
-    return file_path.exists()
+def _input_file_exists(year: int, day: int, part: int, sub_part: str) -> bool:
+    return Path(
+        os.environ["SUGARPLUM_BASE_PATH"] + f"/{year}/{day:02}/test-{part}{sub_part}.in"
+    ).exists()
 
 
-def _read_file(file_path: str, cast_to: Any = str) -> str:
-    with open(os.environ["SUGARPLUM_BASE_PATH"] + "/" + file_path) as f:
-        return cast_to(f.read())
+def _read_file(year: int, day: int, filename: str) -> str:
+    with open(os.environ["SUGARPLUM_BASE_PATH"] + f"/{year}/{day:02}/{filename}") as f:
+        return f.read()
 
 
 def get_input(year: int, day: int) -> str:
-    return _read_file(f"{year}/{day:02}/data.in")
+    return _read_file(year, day, "data.in")
 
 
-def _get_single_test_data(
-    year: int, day: int, part: int, cast_to: Any
-) -> tuple[str, Any]:
-    input_data: str = _read_file(f"{year}/{day:02}/test-{part}.in")
-    result: Any = _read_file(f"{year}/{day:02}/test-{part}.out")
-
-    return (input_data, cast_to(result))
+def get_test_input(year: int, day: int, part: int) -> str:
+    return _read_file(year, day, f"test-{part}.in")
 
 
-def _get_multiple_test_data(year, day, part, cast_to: Any) -> list[tuple[str, Any]]:
+def get_test_answer(year: int, day: int, part: int) -> int:
+    return int(_read_file(year, day, f"test-{part}.out"))
+
+
+def get_parametrized_test_data(year: int, day: int, part: int) -> list[tuple[str, int]]:
     sub_part: str = "a"
-    test_data: list[tuple[str, str]] = []
-    while _file_exists(year, day, part, sub_part):
-        test_data.append(
+    data: list[tuple[str, int]] = []
+    while _input_file_exists(year, day, part, sub_part):
+        data.append(
             (
-                _read_file(f"{year}/{day:02}/test-{part}{sub_part}.in"),
-                cast_to(_read_file(f"{year}/{day:02}/test-{part}{sub_part}.out")),
+                _read_file(year, day, f"test-{part}{sub_part}.in"),
+                int(_read_file(year, day, f"test-{part}{sub_part}.out")),
             )
         )
         sub_part = chr(ord(sub_part) + 1)
 
-    return test_data
-
-
-def get_test_data(
-    year: int, day: int, part: int, cast_to: Any = str
-) -> tuple[str, str] | list[tuple[str, str]]:
-    if _file_exists(year, day, part):
-        return _get_single_test_data(year, day, part, cast_to)
-    else:
-        return _get_multiple_test_data(year, day, part, cast_to)
+    return data
--- tests/test_core.py
+++ tests/test_core.py
@@ -3,7 +3,12 @@ from typing import Callable
 
 import pytest
 
-from sugarplum import get_input, get_test_data
+from sugarplum import (
+    get_input,
+    get_parametrized_test_data,
+    get_test_answer,
+    get_test_input,
+)
 
 YEAR: int = 2023
 DAY: int = 24
@@ -30,55 +35,51 @@ def create_files(tmp_path: Path) -> Callable:
     return _create_files
 
 
-def create_file(path: Path, data: str) -> None:
-    with open(path, "w") as f:
-        f.write(data)
-
-
-def test_puzzle_input_data_is_read(tmp_path: Path, create_files: Callable) -> None:
+def test_reading_input_data(create_files: Callable) -> None:
     expected: str = "North Pole"
     create_files([("data.in", expected)])
 
     assert get_input(YEAR, DAY) == expected
 
 
-def test_puzzle_day_number_gets_zero_padded(tmp_path: Path) -> None:
+def test_day_number_gets_zero_padded(tmp_path: Path) -> None:
     expected: str = "sleigh"
     day: int = 3
-    dir_path: Path = tmp_path / f"{YEAR}/{day:02}"
-    dir_path.mkdir()
+    (tmp_path / f"{YEAR}/{day:02}").mkdir()
     (tmp_path / f"{YEAR}/{day:02}/data.in").write_text(expected, encoding="utf-8")
 
     assert get_input(YEAR, day) == expected
 
 
-def test_get_test_data_retrieves_input_and_output_data_for_single_test_case(
-    tmp_path: Path, create_files: Callable
-) -> None:
-    expected_input: str = "Christmas"
-    expected_output: str = "Tree"
-    create_files([("test-1.in", expected_input), ("test-1.out", expected_output)])
+def test_reading_test_input_data(create_files: Callable) -> None:
+    expected: str = "Christmas"
+    create_files([("test-1.in", expected)])
 
-    assert get_test_data(YEAR, DAY, 1) == (expected_input, expected_output)
+    assert get_test_input(YEAR, DAY, 1) == expected
 
 
-def test_get_test_data_retrieves_multiple_input_and_output_data_for_multiple_test_cases(
-    tmp_path: Path, create_files: Callable
-) -> None:
-    expected_input_a: str = "Santa"
-    expected_output_a: str = "Clause"
-    expected_input_b: str = "Kris"
-    expected_output_b: str = "Kringle"
+def test_reading_test_answer(create_files: Callable) -> None:
+    answer: int = 42
+    create_files([("test-1.out", str(answer))])
+
+    assert get_test_answer(YEAR, DAY, 1) == answer
+
+
+def test_getting_parametrized_data(create_files: Callable) -> None:
+    input_a: str = "Santa"
+    input_b: str = "Claus"
+    output_a: int = 4
+    output_b: int = 16
     create_files(
         [
-            ("test-1a.in", expected_input_a),
-            ("test-1a.out", expected_output_a),
-            ("test-1b.in", expected_input_b),
-            ("test-1b.out", expected_output_b),
+            ("test-1a.in", input_a),
+            ("test-1a.out", str(output_a)),
+            ("test-1b.in", input_b),
+            ("test-1b.out", str(output_b)),
         ]
     )
 
-    assert get_test_data(YEAR, DAY, 1) == [
-        (expected_input_a, expected_output_a),
-        (expected_input_b, expected_output_b),
+    assert get_parametrized_test_data(YEAR, DAY, 1) == [
+        (input_a, output_a),
+        (input_b, output_b),
     ]
